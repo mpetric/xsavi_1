d091c0f08d506c455cbb825dda74fe13fe97cd02
mpetric <matt.petric@gmail.com>
2014-10-26 20:39:53 -0400

    Commit 1

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$("#shakeit").click(function () {
+    var leftlength = 75 + Math.floor(Math.random() * 50);
+    var rightlength = 75 + Math.floor(Math.random() * 50);
+    var imagerotate = Math.random() * 360;
+
+    $("#leftleg").css("height", leftlength);
+    $("#rightleg").css("height", rightlength);
+    $("#image").rotate(imagerotate);
+    if ((imagerotate <= 20 && Math.abs(leftlength - rightlength) <= 12) || (imagerotate >= 340 && Math.abs(leftlength - rightlength) <= 12)) {
+        $("body").css("background-color", "#497641");
+        $("h2").text("Robot Status: Ready to Operate");
+    } else {
+        $("body").css("background-color", "#972E2E")
+        $("h2").text("Robot Status: All Shook Up");
+    }
+    console.log(imagerotate);
+    console.log(Math.abs(leftlength - rightlength));
+});
